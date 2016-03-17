50251a9158e37ed0631f295be50c26376ab2b619
mmmpa <mmmpa.mmmpa@gmail.com>
2016-03-18 05:04:48 +0900

    updated

--- config/initializers/penetration.rb
+++ config/initializers/penetration.rb
@@ -1,4 +1,5 @@
 Penetration.configure do
+  preset(:information) { ->(message) { "<script>Notifier.information('#{message}')</script>" } }
   preset(:notify) { ->(message) { "<script>Notifier.notify('#{message}')</script>" } }
   preset(:alert) { ->(message) { "<script>Notifier.alert('#{message}')</script>" } }
 end
